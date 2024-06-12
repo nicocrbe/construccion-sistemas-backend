0dd913bb6c46512ea0fc4be2f0d7b0f937395c2f
Nicolás Bellesi <nicolas.crbe@gmail.com>
2024-06-12 14:35:10 -0300

    Add files via upload

---
+++ config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+static const char* ssid = "motog_nico";
+static const char* password = "asd12345";
+static const char* usernameLogin = "admin";
+static const char* passwordLogin = "admin";
+static const unsigned int tvPowerOnCommand = 0xE0E040BF;
+static const unsigned int tvPowerOffCommand = 0xE0E040BF;
+
+#endif // CONFIG_H
+
